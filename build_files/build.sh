dd9ef9ec8bd02735d3d370194806515771d735e5
Timothy Lyons <tybl@tybl.dev>
2025-06-03 21:09:08 -0400

    Set available packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,9 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 update -y
+dnf5 install -y alacritty cmake dmenu gcc-c++ git git-lfs i3 i3-config-fedora i3status lightdm neovim tmux
+dnf5 clean all
 
 # Use a COPR Example:
 #
